58a6edd73477237612c59413ca778a178f6eca4d
Darius Bowser <134982506+DariusBowser@users.noreply.github.com>
2024-03-05 07:33:03 -0500

    ContactUs page implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,42 @@
-import Navbar from "@/scenes/navbar"
-import { useEffect, useState } from 'react'
-import { SelectedPage } from "@/shared/types";
-import Home from "./scenes/home";
-import Benefits from "@/scenes/benefits";
+import Navbar from "@/scenes/navbar";
+import Home from "@/scenes/home";
 import OurClasses from "@/scenes/ourClasses";
-
-
+import Benefits from "@/scenes/benefits";
+import ContactUs from "@/scenes/contactUs";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "@/shared/types";
 
 function App() {
-
-  const [selectedPage, setSelectedPage]=  useState<string>(SelectedPage.Home);
-
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
   const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
 
   useEffect(() => {
     const handleScroll = () => {
-    if (window.scrollY === 0) {
+      if (window.scrollY === 0) {
         setIsTopOfPage(true);
         setSelectedPage(SelectedPage.Home);
-    }
-    if (window.scrollY !== 0) setIsTopOfPage(false);
-  }
-  window.addEventListener("scrool", handleScroll)
-  return () => window.removeEventListener("scroll", handleScroll)
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  return <div className="app bg-gray-20">
-    <Navbar 
-      isTopOfPage={isTopOfPage}
-      selectedPage={selectedPage}
-      setSelectedPage={setSelectedPage}    
-    />
-
-    <Home setSelectedPage={setSelectedPage}/>
-    <Benefits setSelectedPage={setSelectedPage}/>
-    <OurClasses setSelectedPage={setSelectedPage}/>
-    </div>;
+  return (
+    <div className="app bg-gray-20">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
+    </div>
+  );
 }
 
 export default App;
